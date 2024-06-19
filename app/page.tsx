babcdc2f7ed6de6bc101d092372d048eb99cc7af
tatetatota <167390540+tatetato@users.noreply.github.com>
2024-06-19 03:31:53 +0000

    feat: Update styles  and page.module.css and add WEEKDAYS to COURSES

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,34 @@
 import styles from "./page.module.css";
-import { COURSES } from "./CONSTANTS";
+import { COURSES, WEEKDAYS } from "./CONSTANTS";
 
 import Course from "./components/Course";
 
 export default function Home() {
   return (
     <main className={"p-4"}>
-      <div className="flex flex-col gap-2">
-        {COURSES.map((item) => {
+      <div className="flex gap-2">
+        {WEEKDAYS.map((item) => {
           return (
-            <Course
-              key={item.id}
-              subject={item.subject}
-              teacher={item.teacher}
-              period={item.period}
-              weekdays={item.weekdays}
-              classroom={item.classroom}
-            />
+            <div key={item.id} className="min-w-[100vw] p-2">
+              <div className="">{item.japanese}</div>
+
+              <div className="flex flex-col gap-2">
+                {COURSES.filter((course) => course.weekdays === item.id).map(
+                  (item) => {
+                    return (
+                      <Course
+                        key={item.id}
+                        subject={item.subject}
+                        teacher={item.teacher}
+                        period={item.period}
+                        weekdays={item.weekdays}
+                        classroom={item.classroom}
+                      />
+                    );
+                  },
+                )}
+              </div>
+            </div>
           );
         })}
       </div>
